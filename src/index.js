9a54da116704ce413a4dfba709fac3c199c21631
philopian <philopian@users.noreply.github.com>
2020-10-22 11:22:05 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>CSS Pulse Effect</h1>
+
+<div class="blobs-container">
+    <div class="blob white"></div>
+    <div class="blob red"></div>
+    <div class="blob orange"></div>
+    <div class="blob yellow"></div>
+    <div class="blob blue"></div>
+    <div class="blob green"></div>
+    <div class="blob purple"></div>
+    <div class="blob"></div>
+</div>
+
+
+`;
